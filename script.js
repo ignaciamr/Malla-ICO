d9ed5185c93c794bfb79f5a7438074dc5b1fd02a
ignaciamr <maitemoreno.as@gmail.com>
2026-01-12 20:47:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const prerequisitos = {
+  mate2: ["mate1"],
+  estad1: ["prog", "mate2"]
+};
+
 function marcar(ramo) {
+  const id = ramo.dataset.ramo;
+
+  // revisar si tiene prerrequisitos
+  if (prerequisitos[id]) {
+    for (let req of prerequisitos[id]) {
+      const ramoReq = document.querySelector(
+        `.ramo[data-ramo="${req}"]`
+      );
+      if (!ramoReq.classList.contains("aprobado")) {
+        alert("Debes aprobar los ramos previos primero");
+        return;
+      }
+    }
+  }
+
   ramo.classList.toggle("aprobado");
 }
